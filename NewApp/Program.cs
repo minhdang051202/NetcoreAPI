95a0fe3ef1265240af6c5cff1913f027530c1f7e
minhdang051202 <nguyenminhdang3638@gmail.com>
2023-09-07 18:04:31 +0700

    baitap

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("minhdang");
+
+// khai bao ho va ten
+const string hoTen ="Nguyen Minh Dang";
+const int tuoi = 21;
+Console.WriteLine("{0} , {1}" , hoTen, tuoi);
+
+
